fc02cde59828c1fdf1ba28fa042a4a18a51c7972
Gavin Cosgreave <39494540+gcosgreave-us@users.noreply.github.com>
2024-03-22 10:42:26 +0000

    adds member.js

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = {
+        name: 'Kenny',
+        age: 25,
+        skills: ['JavaScript', 'React', 'Node', 'Python']
+    };
+    return member.skills;
+}
