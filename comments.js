85bfe96ad36cd7e526aeeafe0043bb975b81bd02
Gavin Cosgreave <39494540+gcosgreave-us@users.noreply.github.com>
2024-03-22 11:06:30 +0000

    Adds new webserver

--- comments.js
+++ comments.js
@@ -1,67 +1,26 @@
-// create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
+// Create web server with express
+// Create a route that accepts GET requests to /comments
+// The route should read the comments from the file comments.json
+// The comments should be returned as JSON
 
-// create web server
-http.createServer(function(request, response){
-  var path = url.parse(request.url).pathname;
-  console.log(path);
-  // if request is GET
-  if (request.method == 'GET') {
-    if (path == '/comments') {
-      // read comments file
-      fs.readFile('comments.json', function(err, data){
-        if (err) {
-          response.writeHead(500, {'Content-Type':'text/plain'});
-          response.end('Internal Server Error');
-          return;
-        }
-        response.writeHead(200, {'Content-Type':'text/plain'});
-        response.end(data);
-      });
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/comments', (req, res) => {
+  const commentsPath = path.join(__dirname, 'comments.json');
+  fs.readFile(commentsPath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('An error occurred while reading the comments file.');
     } else {
-      response.writeHead(404, {'Content-Type':'text/plain'});
-      response.end('Not Found');
+      res.json(JSON.parse(data));
     }
-  // if request is POST
-  } else if (request.method == 'POST') {
-    if (path == '/comments') {
-      var body = '';
-      request.on('data', function(data){
-        body += data;
-      });
-      request.on('end', function(){
-        var post = qs.parse(body);
-        // read comments file
-        fs.readFile('comments.json', function(err, data){
-          if (err) {
-            response.writeHead(500, {'Content-Type':'text/plain'});
-            response.end('Internal Server Error');
-            return;
-          }
-          var comments = JSON.parse(data);
-          comments.push(post);
-          // write comments file
-          fs.writeFile('comments.json', JSON.stringify(comments), function(err){
-            if (err) {
-              response.writeHead(500, {'Content-Type':'text/plain'});
-              response.end('Internal Server Error');
-              return;
-            }
-            response.writeHead(200, {'Content-Type':'text/plain'});
-            response.end('OK');
-          });
-        });
-      });
-    } else {
-      response.writeHead(404, {'Content-Type':'text/plain'});
-      response.end('Not Found');
-    }
-  } else {
-    response.writeHead(405, {'Content-Type':'text/plain'});
-    response.end('Method Not Allowed');
-  }
-}).listen(8080);
-console.log('Server running at http://');
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
